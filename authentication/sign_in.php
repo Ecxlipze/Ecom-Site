2bfa41afd7f88864e9c19df9347c554265b20050
SyedHammadSherazi <144503092+SyedHammadSherazi@users.noreply.github.com>
2025-10-16 10:53:57 +0500

    Update sign_in.php

--- authentication/sign_in.php
+++ authentication/sign_in.php
@@ -1,10 +1,9 @@
 <?php
-$fName=$_POST['fname'];
-$Email=$_POST['email'];
-$password=$_POST['password'];
-echo '$fName';
-echo '$Email';
-echo '$password';
+$fName = $_POST['fname'];      
+$Email = $_POST['email'];      
+$password = $_POST['password']; 
 
-
+echo $fName;     
+echo $Email;     
+echo $password;  
 ?>
